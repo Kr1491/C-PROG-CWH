5d17c398fd397a487618f640694ea9064b17d38b
Krishna Jha <jhakrishna1491@gmail.com>
2024-08-15 00:23:52 +0530

    Completed Prob1

---
+++ 1_Code with Harry Oneshot C PROG/PRACTICE-SET/practice-set-3/prob1.c
@@ -0,0 +1,17 @@
+// #include <stdio.h>
+
+// int main(int argc, char const *argv[])
+// {
+//   int a = 10;
+//   if (a = 11)
+//   {
+//     printf("I am 11");
+//   }
+//   else{
+//     printf("I am 10");
+//   }
+//   return 0;
+// }
+
+//Output: I am 11, because the variable is declared again in the if
+//statement
