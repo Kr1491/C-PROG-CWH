43f4f7487bfdf9b8169123a14d1732c664c98f2c
Krishna Jha <jhakrishna1491@gmail.com>
2024-08-15 20:09:48 +0530

    Add Input to Project 1

--- 1_Code with Harry Oneshot C PROG/REVISION/pyramid_pattern.c
+++ 1_Code with Harry Oneshot C PROG/REVISION/pyramid_pattern.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 int main()
-{
-  for (int i = 1; i <= 10; i++)
+{ 
+  int ans;
+  printf(">>");
+  scanf("%d",&ans);
+
+
+  for (int i = 1; i <= ans; i++)
   {
     for (int j = 1; j <= i; j++)
     {
