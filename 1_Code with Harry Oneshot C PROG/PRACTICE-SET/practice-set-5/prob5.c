b2e1655651b6423edd222fe2a77edeb6b812544e
Krishna Jha <jhakrishna1491@gmail.com>
2024-08-16 21:24:47 +0530

    Completed Prob 5

---
+++ 1_Code with Harry Oneshot C PROG/PRACTICE-SET/practice-set-5/prob5.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int fib(int n){
+  if (n == 0)
+  {
+    return 0;
+  }
+  
+  else if (n == 1)
+  {
+    return 1;
+  }
+  
+  else{
+    return fib(n-1) + fib(n-2);
+  }
+}
+int main()
+{
+  int a;
+  printf("Ent>>");
+  scanf("%d",&a);
+
+  printf("%d",fib(a));
+
+  
+  return 0;
+}
