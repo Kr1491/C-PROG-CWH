896015cc80a83e9d5d2ff645ac486b513e53fcd2
Krishna Jha <jhakrishna1491@gmail.com>
2024-08-19 16:34:40 +0530

    Completed Prob3

---
+++ 1_Code with Harry Oneshot C PROG/PRACTICE-SET/practice-set-6/prob3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int tentimes(int* n){
+  return *n*10; 
+}
+
+int main()
+{
+  int i;
+  printf("Enter>> ");
+  scanf("%d",&i);
+
+  printf("Value of ten times of input is %d", tentimes(&i));
+  return 0;
+}
