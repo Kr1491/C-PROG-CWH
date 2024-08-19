9bfc6385a61e5ebec998b193fc1501e9b74cf38c
Krishna Jha <jhakrishna1491@gmail.com>
2024-08-19 16:22:51 +0530

    Completed Prob1

--- 1_Code with Harry Oneshot C PROG/CODE/chapter-6/swap_num_pointer.c
+++ 1_Code with Harry Oneshot C PROG/CODE/chapter-6/swap_num_pointer.c
@@ -1,6 +1,7 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void swap(int *a, int *b){
+void swap(int *a, int *b)
+{
   int temp;
   temp = *a;
   *a = *b;
@@ -11,13 +12,13 @@ int main()
 {
   int a, b;
   printf("First Number is: ");
-  scanf("%d",&a);
+  scanf("%d", &a);
 
   printf("Second Number is: ");
-  scanf("%d",&b);
+  scanf("%d", &b);
 
-  swap(&a,&b);
+  swap(&a, &b);
 
-  printf("Now First Number is: %d and Second Number is: %d",a,b);
+  printf("Now First Number is: %d and Second Number is: %d", a, b);
   return 0;
 }
---
+++ 1_Code with Harry Oneshot C PROG/PRACTICE-SET/practice-set-6/prob1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+  
+  int a = 6;
+  int* temp = &a;
+  printf("Value of a %d",*temp);
+  return 0;
+} 
